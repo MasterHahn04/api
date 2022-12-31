ef9e866233453ad8bc9ffba6b68d5d3fb33a955d
MasterHahn04 <git@t-hahn.eu>
2022-12-31 13:25:36 +0100

    ^Vollständig

--- api.go
+++ api.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"log"
 	"net/http"
+	"strconv"
 	"teamspeakapi/teamspeak"
 )
 
@@ -22,12 +23,43 @@ func main() {
 	ts.GET("/clientlist", func(c *gin.Context) {
 		clientList, err := teamspeak.ClientList(client)
 		if err != nil {
-			log.Fatalln("Error Get ClientList: ", err)
+			log.Println("Error Get ClientList: ", err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		}
 		c.JSON(http.StatusOK, clientList)
 	})
-	ts.GET("/whowhere", func(c *gin.Context) {
-		teamspeak.WhoWhere(client)
+	{
+		ts.GET("/whowhere", func(c *gin.Context) {
+			whoWhere, err := teamspeak.WhoWhere(client)
+			if err != nil {
+				log.Println("Error Get WhoWhere:", err)
+				c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			}
+			c.JSON(http.StatusOK, whoWhere)
+		})
+		ts.GET("/whowhere/short", func(c *gin.Context) {
+			whoWhereShort, err := teamspeak.WhoWhereShort(client)
+			if err != nil {
+				log.Println("Error Get WhoWhereShort:", err)
+				c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			}
+			c.JSON(http.StatusOK, whoWhereShort)
+		})
+	}
+
+	ts.GET("/channelinfo/:id", func(c *gin.Context) {
+		var channelInfo teamspeak.ChannelInfoStruct
+		if number, err := strconv.Atoi(c.Param("id")); err != nil {
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		} else {
+			channelInfo, err = teamspeak.ChannelInfo(client, number)
+			if err != nil {
+				log.Println("Error Get ChannelInfo:", err)
+				c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			}
+		}
+		c.JSON(http.StatusOK, channelInfo)
 	})
 	r.Run(":80")
 }
--- teamspeak/ts.go
+++ teamspeak/ts.go
@@ -52,34 +52,91 @@ type ClientListStruct struct {
 	AwayMessage  string `json:"away_message"`
 }
 
-func WhoWhere(client *ts3.Client) {
+func WhoWhere(client *ts3.Client) ([]WhoWhereStruct, error) {
+	var whoWhereList []WhoWhereStruct
 	clientList, err := ClientList(client)
 	if err != nil {
-
+		return nil, err
 	}
 	for _, entry := range clientList {
-		if entry.Type == 1 {
-
+		if entry.Type == 0 {
+			var clientInfoWithChannel WhoWhereStruct
+			clientInfoWithChannel.ChannelInfo, err = ChannelInfo(client, entry.ChannelId)
+			if err != nil {
+				return nil, err
+			}
+			clientInfoWithChannel.ClientList = entry
+			whoWhereList = append(whoWhereList, clientInfoWithChannel)
 		} else {
 			continue
 		}
 	}
+	return whoWhereList, nil
 }
 
-type WhoWhereStruct struct {
-	ClientList ClientListStruct
-}
-
-func ChannelInfo(client *ts3.Client, channelId int) {
-	list, err := client.ExecCmd(ts3.NewCmd(fmt.Sprintf("channelinfo cid=%v", channelId)).WithResponse())
+func WhoWhereShort(client *ts3.Client) ([]WhoWhereShortStruct, error) {
+	var whoWhereShortList []WhoWhereShortStruct
+	whoWhereList, err := WhoWhere(client)
 	if err != nil {
-
+		return nil, err
 	}
-
+	for _, entry := range whoWhereList {
+		shortList := WhoWhereShortStruct{
+			Nickname:    entry.ClientList.Nickname,
+			ChannelName: entry.ChannelInfo.Name,
+		}
+		whoWhereShortList = append(whoWhereShortList, shortList)
+	}
+	return whoWhereShortList, nil
 }
 
-type ChannelInfo struct {
-	P_ID int `ms:"pid" json:"p_ID"`
+type WhoWhereShortStruct struct {
+	Nickname    string `json:"nickname"`
+	ChannelName string `json:"channel_name"`
+}
+type WhoWhereStruct struct {
+	ClientList  ClientListStruct  `json:"client_list"`
+	ChannelInfo ChannelInfoStruct `json:"channel_info"`
+}
 
-	pid=19 channel_name=Mannschaftsheim\sV channel_topic channel_description channel_password channel_codec=4 channel_codec_quality=6 channel_maxclients=-1 channel_maxfamilyclients=-1 channel_order=23 channel_flag_permanent=1 channel_flag_semi_permanent=0 channel_flag_default=0 channel_flag_password=0 channel_codec_latency_factor=1 channel_codec_is_unencrypted=0 channel_security_salt channel_delete_delay=0 channel_unique_identifier=0fe7005e-c197-419e-aca8-f88adc11ed53 channel_flag_maxclients_unlimited=1 channel_flag_maxfamilyclients_unlimited=1 channel_flag_maxfamilyclients_inherited=0 channel_filepath=files\/virtualserver_1\/channel_24 channel_needed_talk_power=0 channel_forced_silence=0 channel_name_phonetic=Mannschaftsheim\s5 channel_icon_id=2053633368 channel_banner_gfx_url channel_banner_mode=0 seconds_empty=785237
+func ChannelInfo(client *ts3.Client, channelId int) (ChannelInfoStruct, error) {
+	var channelInfos ChannelInfoStruct
+	_, err := client.ExecCmd(ts3.NewCmd(fmt.Sprintf("channelinfo cid=%v", channelId)).WithResponse(&channelInfos))
+	if err != nil {
+		return ChannelInfoStruct{}, err
+	}
+	return channelInfos, nil
+}
+
+type ChannelInfoStruct struct {
+	P_ID                          int    `ms:"pid" json:"p_id"`
+	Name                          string `ms:"channel_name" json:"name"`
+	Topic                         string `ms:"channel_topic" json:"topic"`
+	Description                   string `ms:"channel_description" json:"description"`
+	Password                      string `ms:"channel_password" json:"password"`
+	Codec                         int    `ms:"channel_codec" json:"codec"`
+	CodecQuality                  int    `ms:"channel_codec_quality" json:"codec_quality"`
+	MaxClients                    int    `ms:"channel_maxclients" json:"max_clients"`
+	MaxFamilyClients              int    `ms:"channel_maxfamilyclients" json:"max_family_clients"`
+	Order                         int    `ms:"channel_order" json:"order"`
+	FlagPermanent                 int    `ms:"channel_flag_permanent" json:"flag_permanent"`
+	FlagSemiPermanent             int    `ms:"channel_flag_semi_permanent" json:"flag_semi_permanent"`
+	FlagDefault                   int    `ms:"channel_flag_default" json:"flag_default"`
+	FlagPassword                  int    `ms:"channel_flag_password" json:"flag_password"`
+	CodecLatencyFactor            int    `ms:"channel_codec_latency_factor" json:"codec_latency_factor"`
+	CodecIsUnencrypted            int    `ms:"channel_codec_is_unencrypted" json:"codec_is_unencrypted"`
+	SecuritySalt                  string `ms:"channel_security_salt" json:"security_salt"`
+	DeleteDelay                   int    `ms:"channel_delete_delay" json:"delete_delay"`
+	UniqueIdentifier              string `ms:"channel_unique_identifier" json:"unique_identifier"`
+	FlagMaxClientsUnlimited       int    `ms:"channel_flag_maxclients_unlimited" json:"flag_max_clients_unlimited"`
+	FlagMaxFamilyClientsUnlimited int    `ms:"channel_flag_maxfamilyclients_unlimited" json:"flag_max_family_clients_unlimited"`
+	FlagMaxFamilyClientsInherited int    `ms:"channel_flag_maxfamilyclients_inherited" json:"flag_max_family_clients_inherited"`
+	Filepath                      string `ms:"channel_filepath" json:"filepath"`
+	NeededTalkPower               int    `ms:"channel_needed_talk_power" json:"needed_talk_power"`
+	ForcedSilence                 int    `ms:"channel_forced_silence" json:"forced_silence"`
+	NamePhonetic                  string `ms:"channel_name_phonetic" json:"name_phonetic"`
+	IconId                        int    `ms:"channel_icon_id" json:"icon_id"`
+	BannerGfxUrl                  string `ms:"channel_banner_gfx_url" json:"banner_gfx_url"`
+	BannerMode                    int    `ms:"channel_banner_mode" json:"banner_mode"`
+	SecondsEmpty                  int    `ms:"seconds_empty" json:"seconds_empty"`
 }
